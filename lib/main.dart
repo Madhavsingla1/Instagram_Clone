a70b5aeb5b3660dd2aa5a4fa36c45b2242cd7f07
Madhavsingla1 <msingla_be19@thapar.edu>
2022-07-12 18:14:48 +0530

    Adding new files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:insta_clone/responsive/mobile_screen_layout.dart';
 import 'package:insta_clone/responsive/responsive_layout_screen.dart';
 import 'package:insta_clone/responsive/web_screen_layout.dart';
+import 'package:insta_clone/screens/login_screen.dart';
 import 'package:insta_clone/utils/colors.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  // Makes sure the flutter widgets are intialized before the intialization of firebase
+  WidgetsFlutterBinding.ensureInitialized();
+  if (kIsWeb) {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: 'AIzaSyAuVQf2hK5oLAIDuDgvs6NjNaPAuBgzLes',
+        appId: '1:861840785097:web:b2d37a4ef42fdbbd1a9718',
+        messagingSenderId: '861840785097',
+        projectId: 'instagram-clone-c6301',
+        storageBucket: 'instagram-clone-c6301.appspot.com',
+      ),
+    );
+  } else {
+    await Firebase.initializeApp();
+  }
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   Key? get key => super.key;
@@ -15,10 +35,11 @@ class MyApp extends StatelessWidget {
       title: 'Instagram Clone',
       theme: ThemeData.dark()
           .copyWith(scaffoldBackgroundColor: mobileBackgroundColor),
-      home: ResposiveLayout(
-        mobileScreenLayout: MobileScreenLayout(),
-        webScreenLayout: WebScreenLayout(),
-      ),
+      // home: ResposiveLayout(
+      //   mobileScreenLayout: MobileScreenLayout(),
+      //   webScreenLayout: WebScreenLayout(),
+      // ),
+      home: LoginScreen(),
     );
   }
 }
